8967871d63f62a680326abb7af8eff8981a227df
Pierrick Rouxel <pierrickrouxel95@gmail.com>
2026-04-04 10:10:26 +0200

    Ajout de la page de la boutique

--- MaBrasserieIncrementale/src/components/Shop.tsx
+++ MaBrasserieIncrementale/src/components/Shop.tsx
@@ -1,15 +1,17 @@
 import { useGameContext } from "../context/GameContext";
 import boutiqueImage from "../assets/boutique.jpg";
+import { useNavigate } from "react-router-dom";
 
 export default function Shop() {
   const { shopUnlocked } = useGameContext();
+  const navigate = useNavigate();
 
   if (!shopUnlocked) return null; // Ne s'affiche pas tant que shopUnlocked = false
 
   return (
     <div className="shop">
       <img src={boutiqueImage} alt="Boutique" className="shop-image" />
-      <button>Boutique</button>
+      <button onClick={() => navigate("/shop")}>Boutique</button>
     </div>
   );
 }
---
+++ MaBrasserieIncrementale/src/pages/ShopPage.tsx
@@ -0,0 +1,9 @@
+export default function ShopPage() {
+  return (
+    <div className="shop-page">
+      <h1>Bienvenue à la Boutique !</h1>
+      <p>Ici, vous pouvez acheter des améliorations pour votre brasserie.</p>
+      {/* Contenu de la boutique à ajouter */}
+    </div>
+  );
+}
